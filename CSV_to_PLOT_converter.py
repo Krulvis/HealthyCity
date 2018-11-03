fc9dfc719db676f6233e2d35f127ff4fc7fadabf
Noah Lammertsma <noah5073@gmail.com>
2018-11-03 10:36:21 +0100

    update csv plot 3

--- CSV_to_PLOT_converter.py
+++ CSV_to_PLOT_converter.py
@@ -18,8 +18,56 @@ from mpl_toolkits.basemap import Basemap
 import numpy as np
 
 
-# In[37]:
+# In[48]:
 
+
+def buurtNaam(buurtNaam , particle):
+    if "Vondelpark" in buurtNaam: 
+        data = createFuture(['Amsterdam-Vondelpark'], particle)
+    else:
+        data = createFuture(['Amsterdam-Stadhouderskade'], particle)
+    #sort values by day (tijdstip)
+    values = data[0].sort_values(by=['tijdstip'])
+
+    #get lenght of the value array
+    length = len(values)
+
+
+
+    i = 0
+    #convert value to CORRECT datatypes / split strings where necessary 
+
+    tijdstip = values['tijdstip']
+    values['tijdstip'] = [w.split(' ', 1)[0] for w in values['tijdstip']]
+    problem = values['waarde']
+
+    values['waarde'] = [w.replace(',' , '.') for w in values['waarde']]
+    problem = pd.to_numeric(values['waarde'])
+
+    #create plot x & y array
+    time = [] # x
+    problemVal=[] # y
+
+    #fill set (x & y ) arrays
+    while i < length :
+        if i % 24 == 0:
+            time.append(tijdstip.iloc[i])
+            problemVal.append(problem.iloc[i])
+        i += 1   
+
+    #create plot    
+    fig, ax = plt.subplots()
+
+    # fill plot with x & y arrays
+    ax.plot(time , problemVal)
+    # set Labels
+    ax.set(xlabel='Day', ylabel=values['component'].iloc[0],
+           title= values['component'].iloc[0] + " in the air")
+
+    my_xticks = ax.get_xticks()
+    plt.xticks([my_xticks[0], my_xticks[-1]], visible=True, rotation="horizontal")
+
+    return plt.show()
 
 def createFuture(filters , problem): 
     
@@ -38,61 +86,9 @@ def createFuture(filters , problem):
     #return set array
     return returnVals
 
-# create custom data request
-if "Vondelpark" in buurt_naam: 
-    data = createFuture(['Amsterdam-Vondelpark'], 'NO2')
-    continue
-    
-if "Vondelpark" not in buurt_naam:
-    data = createFuture(['Amsterdam-Stadhouderskade'], 'NO2')
-    continue
 
 
 
-#sort values by day (tijdstip)
-values = data[0].sort_values(by=['tijdstip'])
-
-#get lenght of the value array
-length = len(values)
-
-
-
-i = 0
-#convert value to CORRECT datatypes / split strings where necessary 
-
-tijdstip = values['tijdstip']
-values['tijdstip'] = [w.split(' ', 1)[0] for w in values['tijdstip']]
-problem = values['waarde']
-
-values['waarde'] = [w.replace(',' , '.') for w in values['waarde']]
-problem = pd.to_numeric(values['waarde'])
-
-#create plot x & y array
-time = [] # x
-problemVal=[] # y
-
-#fill set (x & y ) arrays
-while i < length :
-    if i % 24 == 0:
-        time.append(tijdstip.iloc[i])
-        problemVal.append(problem.iloc[i])
-    i += 1   
-
-#create plot    
-fig, ax = plt.subplots()
-
-# fill plot with x & y arrays
-ax.plot(time , problemVal)
-# set Labels
-ax.set(xlabel='Day', ylabel=values['component'].iloc[0],
-       title= values['component'].iloc[0] + " in the air")
-
-my_xticks = ax.get_xticks()
-plt.xticks([my_xticks[0], my_xticks[-1]], visible=True, rotation="horizontal")
-
-
-# show the plot
-plt.show()
 
 
 
@@ -102,8 +98,8 @@ plt.show()
 plt.style.use('ggplot')
 
 
-# In[ ]:
+# In[53]:
 
 
-
+buurtNaam('Vondelpark', 'O3')
 
